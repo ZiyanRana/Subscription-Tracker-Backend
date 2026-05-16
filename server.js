cae135d6cfb2b57fdec2ba022c5639fa316cbee2
ZiyanRana <mziyanrana@gmail.com>
2026-05-16 16:28:52 +0500

    Working on project: Subscription Tracker

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import app from './src/app.js';
 import {PORT} from './src/config/env.js';
+import connectToDb from './src/database/mongodb.js';
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    await connectToDb();
 })
--- src/config/env.js
+++ src/config/env.js
@@ -6,5 +6,6 @@ config (
 
 export const {
     PORT, 
-    NODE_ENV
+    NODE_ENV,
+    DB_URI
 } = process.env;
