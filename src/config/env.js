da1021a405a20cb7746a25ad594e6cc1d12aad0e
ZiyanRana <mziyanrana@gmail.com>
2026-05-17 23:42:27 +0500

    Working on project: Subscription Tracker

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 
 config ( 
-    {path: `.env.${process.env.NODE_ENV || 'development'}`
+    { path: `.env.${process.env.NODE_ENV || 'development' }`
 });
 
 export const {
--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import {DB_URI, NODE_ENV} from "../config/env.js";
 
 if(!DB_URI) {
